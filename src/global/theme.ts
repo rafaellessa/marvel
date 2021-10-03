264ac1bf6201ac8680296150270cd85ecd606f0a
Rafael Lessa <rafael.lessa150@gmail.com>
2021-10-03 01:51:00 -0300

    feat: create theme template

---
+++ src/global/theme.ts
@@ -0,0 +1,17 @@
+export default {
+  colors: {
+    primary: "#FF031A",
+    secondary: "#000",
+    white: "#fff",
+
+    gradientPrimary: "#000000",
+    gradientSecondary: "#4F0904",
+    gradientTerciary: "#000000",
+  },
+  fonts: {
+    regular: "Poppins_400Regular",
+    medium: "Poppins_500Medium",
+    semiBold: "Poppins_600SemiBold",
+    bold: "Poppins_700Bold",
+  },
+};
