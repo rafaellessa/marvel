00b264f198bdeb8cdf30f59e41d2b4b57d354b37
Rafael Lessa <rafael.lessa150@gmail.com>
2021-10-03 01:51:24 -0300

    feat: create app

--- App.tsx
+++ App.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { ThemeProvider } from "styled-components/native";
+import Background from "./src/components/Background";
+import theme from "./src/global/theme";
+import Splash from "./src/screens/Splash";
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
 
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
 
 const App: React.FC = () => {
-  return <View />;
-}
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Background>
+        <Splash />
+      </Background>
+    </ThemeProvider>
+  );
+};
 
 export default App;
