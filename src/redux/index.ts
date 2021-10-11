fe7e12a1000486cc5056dc0aabc18c6bd1c47379
Rafael Lessa <rafael.lessa150@gmail.com>
2021-10-11 04:42:20 -0300

    feat: config redux

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -2,15 +2,18 @@ import { all } from "@redux-saga/core/effects";
 import { Reducer } from "react";
 import { combineReducers } from "redux";
 import { reducer as reducerCharacters } from "./reducers/reducers.characters";
+import { reducer as reducerAuth } from "./reducers/reducers.auth";
 import { AppReducers } from "./types/types.redux";
 import sagasCharacters from "./sagas/sagas.characters";
+import sagasAuth from "./sagas/sagas.auth";
 
 const rootReducers = combineReducers({
   [AppReducers.characters]: reducerCharacters,
+  [AppReducers.auth]: reducerAuth,
 });
 
 export const rootSagas = function* rootSagas(): Generator {
-  return yield all([sagasCharacters()]);
+  return yield all([sagasCharacters(), sagasAuth()]);
 };
 
 interface AppRedux {
