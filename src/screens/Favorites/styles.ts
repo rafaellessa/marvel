0009e89b2cc078e00ba93659a6134faf30f14ffa
Rafael Lessa <rafael.lessa150@gmail.com>
2021-10-10 21:09:09 -0300

    feat: create Favorites screen

---
+++ src/screens/Favorites/styles.ts
@@ -0,0 +1,7 @@
+import { RFValue } from "react-native-responsive-fontsize";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+`;
