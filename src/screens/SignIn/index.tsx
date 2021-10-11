14fae6d9ee1cc6a18015aade4c43bad1cddc4998
Rafael Lessa <rafael.lessa150@gmail.com>
2021-10-10 21:09:49 -0300

    feat: create Singin screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -5,14 +5,29 @@ import { Container, Footer } from "./styles";
 
 import Google from "../../assets/google_icon.svg";
 import Apple from "../../assets/apple_icon.svg";
+import { useNavigation } from "@react-navigation/native";
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation();
+
+  const handlePressButton = () => {
+    navigation.navigate("Characters");
+  };
+
   return (
     <Container>
       <IronMan />
       <Footer>
-        <SigninButton logo={<Google />} title="Entrar com Google" />
-        <SigninButton logo={<Apple />} title="Entrar com Apple" />
+        <SigninButton
+          logo={<Google />}
+          title="Entrar com Google"
+          onPress={handlePressButton}
+        />
+        <SigninButton
+          logo={<Apple />}
+          title="Entrar com Apple"
+          onPress={handlePressButton}
+        />
       </Footer>
     </Container>
   );
