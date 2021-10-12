f22a2469234a132ad90e806a2ba51fbf03e3517b
Rafael Lessa <rafael.lessa150@gmail.com>
2021-10-12 19:12:41 -0300

    feat: create PageHeader component

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,12 +1,20 @@
+import { useNavigation } from "@react-navigation/core";
 import React from "react";
-import { Container, Title } from "./styles";
+import { Container, Title, IconWrapper, IconArrowBack } from "./styles";
 
 interface PageHeaderProps {
   title: string;
+  rightIcon: boolean;
 }
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ title, rightIcon }) => {
+  const navigation = useNavigation();
   return (
     <Container>
+      {rightIcon && (
+        <IconWrapper onPress={() => navigation.goBack()}>
+          <IconArrowBack />
+        </IconWrapper>
+      )}
       <Title>{title}</Title>
     </Container>
   );
--- src/components/PageHeader/styles.ts
+++ src/components/PageHeader/styles.ts
@@ -1,13 +1,12 @@
 import { getStatusBarHeight } from "react-native-iphone-x-helper";
 import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
-
+import { MaterialIcons } from "@expo/vector-icons";
 export const Container = styled.View`
   flex-direction: row;
   width: 100%;
   margin-top: ${getStatusBarHeight() + RFValue(30)}px;
   padding: 0 24px;
-  justify-content: center;
   align-items: center;
 `;
 
@@ -15,4 +14,14 @@ export const Title = styled.Text`
   color: ${({ theme }) => theme.colors.white};
   font-family: ${({ theme }) => theme.fonts.bold};
   font-size: ${RFValue(18)}px;
+  margin-left: 20px;
+`;
+
+export const IconWrapper = styled.TouchableOpacity``;
+
+export const IconArrowBack = styled(MaterialIcons).attrs({
+  size: 30,
+  name: "arrow-back",
+})`
+  color: ${({ theme }) => theme.colors.white};
 `;
